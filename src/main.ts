b9d1ce989de42595ac0038609ea132c1c84013a8
uxname <uxname@gmail.com>
2026-03-01 12:16:27 +0300

    ⚡️ feat: add --version and -V flags to CLI

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
 import { CommandFactory } from 'nest-commander';
+import packageJson from '../package.json';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  await CommandFactory.run(AppModule);
+  await CommandFactory.run(AppModule, {
+    version: packageJson.version,
+  });
 }
 bootstrap();
